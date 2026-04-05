289cf1e4216381957db9918e28345aee6eb2ebc5
vannak-chan884 <vannakchan884@gmail.com>
2026-04-05 11:39:56 +0700

    Modify emailjs

--- src/router/sendRSVP.js
+++ src/router/sendRSVP.js
@@ -5,10 +5,10 @@ import emailjs from "emailjs-com";
 export const sendRSVP = (formData, onSuccess, onError) => {
   emailjs
     .send(
-      "e-invitation",  // Replace with your EmailJS Service ID
-      "e-invitation", // Replace with your EmailJS Template ID
+      "naknoy",  // Replace with your EmailJS Service ID
+      "naknoy", // Replace with your EmailJS Template ID
       formData,        // ✅ Use formData instead of undefined templateParams
-      "C36o8eMJ0mZJezG-s"   // Replace with your EmailJS Public Key
+      "BDpXdwLquvj4nlQS0"   // Replace with your EmailJS Public Key
     )
     .then(() => {
       onSuccess();
